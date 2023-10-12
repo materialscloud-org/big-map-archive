0d5daeadb40143bfc6b8c3f111d85e6ef3c54cc1
Francois Liot <francois.liot@epfl.ch>
2023-10-12 11:31:47 +0200

    Removed visibility card in upload form

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -2,4 +2,5 @@ import CardDepositStatusBox from "../CardDepositStatusBox";
 
 export const overriddenComponents = {
     "InvenioAppRdm.Deposit.CardDepositStatusBox.container": CardDepositStatusBox,
+    "InvenioAppRdm.Deposit.AccessRightField.container": () => null,
 }
