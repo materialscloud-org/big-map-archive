b766a96822023b23673e31677c73b9171d13a425
Francois Liot <francois.liot@epfl.ch>
2023-10-12 18:53:38 +0200

    Simplified UI for community selection

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -1,6 +1,9 @@
-import CardDepositStatusBox from "../CardDepositStatusBox";
+import BMACardDepositStatusBox from "../BMACardDepositStatusBox";
+import { BMACommunityHeader } from "../BMACommunityHeader";
+import { BMACommunitySelectionSearch } from "../BMACommunitySelectionSearch.js";
 
 export const overriddenComponents = {
-    "InvenioAppRdm.Deposit.CardDepositStatusBox.container": CardDepositStatusBox,
+    "InvenioAppRdm.Deposit.CardDepositStatusBox.container": BMACardDepositStatusBox,
     "InvenioAppRdm.Deposit.AccessRightField.container": () => null,
+    "InvenioAppRdm.Deposit.CommunityHeader.container": BMACommunityHeader,
 }
