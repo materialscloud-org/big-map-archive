21b6042b0ec2e178187b164a9f0115b429971199
Francois Liot <francois.liot@epfl.ch>
2023-10-12 20:28:34 +0200

    Removed unused imports

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -1,6 +1,5 @@
 import BMACardDepositStatusBox from "../BMACardDepositStatusBox";
 import { BMACommunityHeader } from "../BMACommunityHeader";
-import { BMACommunitySelectionSearch } from "../BMACommunitySelectionSearch.js";
 
 export const overriddenComponents = {
     "InvenioAppRdm.Deposit.CardDepositStatusBox.container": BMACardDepositStatusBox,
