bd627a71d35e612fa2005010f90a529ec845620f
Francois Liot <francois.liot@epfl.ch>
2023-10-15 14:16:00 +0200

    Removed publication date field from upload form

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -5,4 +5,5 @@ export const overriddenComponents = {
     "InvenioAppRdm.Deposit.CardDepositStatusBox.container": BMACardDepositStatusBox,
     "InvenioAppRdm.Deposit.AccessRightField.container": () => null,
     "InvenioAppRdm.Deposit.CommunityHeader.container": BMACommunityHeader,
+    "InvenioAppRdm.Deposit.PublicationDateField.container": () => null,
 }
