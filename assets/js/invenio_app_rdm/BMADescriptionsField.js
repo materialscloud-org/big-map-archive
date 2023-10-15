bc6d6683374675621a3a72a5d329b1facbe46494
Francois Liot <francois.liot@epfl.ch>
2023-10-15 18:24:14 +0200

    Removed additional description

---
+++ assets/js/invenio_app_rdm/BMADescriptionsField.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { FieldLabel, RichInputField } from "react-invenio-forms";
+import { i18next } from "@translations/invenio_rdm_records/i18next";
+
+export class BMADescriptionsField extends Component {
+  render() {
+    const { fieldPath, label, labelIcon, options, editorConfig, recordUI } = this.props;
+    return (
+      <>
+        <RichInputField
+          className="description-field rel-mb-1"
+          fieldPath={fieldPath}
+          editorConfig={editorConfig}
+          label={<FieldLabel htmlFor={fieldPath} icon={labelIcon} label={label} />}
+          optimized
+        />
+      </>
+    );
+  }
+}
+
+BMADescriptionsField.propTypes = {
+  fieldPath: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  labelIcon: PropTypes.string,
+  editorConfig: PropTypes.object,
+  recordUI: PropTypes.object,
+  options: PropTypes.object.isRequired,
+};
+
+BMADescriptionsField.defaultProps = {
+  label: i18next.t("Description"),
+  labelIcon: "pencil",
+  editorConfig: undefined,
+  recordUI: undefined,
+};
