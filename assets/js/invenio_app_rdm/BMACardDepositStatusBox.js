5cf866a584867872b928266020e97ee27ffc15a1
Francois Liot <francois.liot@epfl.ch>
2023-12-15 15:37:18 +0100

    Inserted tooltip for upload form

--- assets/js/invenio_app_rdm/BMACardDepositStatusBox.js
+++ assets/js/invenio_app_rdm/BMACardDepositStatusBox.js
@@ -1,14 +1,29 @@
 import React from "react";
 import {
-  DepositStatusBox,
-  SaveButton,
+    DepositStatusBox,
+    SaveButton,
 } from "@js/invenio_rdm_records";
-import { Card, Grid, } from "semantic-ui-react";
-import { BMASubmitReviewOrPublishButton } from "./BMASubmitReviewOrPublishButton";
+import {Card, Grid, Icon, Popup} from "semantic-ui-react";
+import {BMASubmitReviewOrPublishButton} from "./BMASubmitReviewOrPublishButton";
 
 const BMACardDepositStatusBox = () => {
     return (
         <Card>
+            <Card.Content>
+                <Grid verticalAlign="middle">
+                    <Grid.Row centered className="pt-5 pb-5">
+                        <Grid.Column
+                            width="16"
+                            textAlign="center"
+                        >
+                            <Popup
+                                trigger={<Icon className="ml-10" name="info circle"/>}
+                                content='Click "Save draft" to create or update a private record. Click "Share with community" to privately share your record with the selected community.'
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Card.Content>
             <Card.Content>
                 <Grid relaxed>
                     <Grid.Column
