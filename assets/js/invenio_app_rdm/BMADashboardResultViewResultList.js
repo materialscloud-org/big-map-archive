5ad3f3377906beaf5bdfed106ef8c5517f6d23e9
Francois Liot <francois.liot@epfl.ch>
2023-12-15 17:45:29 +0100

    Added tooltip for my records

--- assets/js/invenio_app_rdm/BMADashboardResultViewResultList.js
+++ assets/js/invenio_app_rdm/BMADashboardResultViewResultList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {ResultsList} from "react-searchkit";
-import {Grid} from "semantic-ui-react";
+import {Grid, Popup, Icon} from "semantic-ui-react";
 
 export function BMADashboardResultViewResultList(props) {
     const {id, sortOptions, paginationOptions, currentResultsState, appName} = props;
@@ -9,6 +9,10 @@ export function BMADashboardResultViewResultList(props) {
             <Grid.Column>
                 <div style={{"display": "flex", "align-items": "center"}}>
                     <h2 style={{"margin": "0px"}}>My records</h2>
+                    <Popup
+                        trigger={<Icon className="ml-5" name="info circle"/>}
+                        content={"Use the closest search box to filter through your records. The search guide provides examples of advanced search queries."}
+                    />
                 </div>
                 <ResultsList/>
             </Grid.Column>
