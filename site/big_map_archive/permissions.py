1872288b8e1156bbb6f1527f0885f2b39fd4fb8f
Valeria Granata <valeria@chaw.com>
2024-03-25 20:32:46 +0100

    community policy: moved community permissions out of invenio.cfg * created BMACommunityPermissionPolicy

--- site/big_map_archive/permissions.py
+++ site/big_map_archive/permissions.py
@@ -6,16 +6,21 @@
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Permissions for BIG-MAP Archive Records."""
+from invenio_communities.generators import (CommunityMembers,
+                                            IfCommunityDeleted, IfPolicyClosed)
+from invenio_communities.generators import \
+    IfRestricted as CommunityIfRestricted
+from invenio_communities.permissions import CommunityPermissionPolicy
 from invenio_rdm_records.services.generators import (
     CommunityInclusionReviewers, IfFileIsLocal, IfRestricted,
-    RecordCommunitiesAction, RecordOwners, ResourceAccessToken, SecretLinks,
-    SubmissionReviewer)
+    RecordCommunitiesAction, RecordOwners, ResourceAccessToken, SecretLinks)
 from invenio_rdm_records.services.permissions import RDMRecordPermissionPolicy
-from invenio_records_permissions.generators import (AuthenticatedUser,
+from invenio_records_permissions.generators import (AuthenticatedUser, Disable,
                                                     IfConfig, SystemProcess)
 from invenio_users_resources.services.permissions import UserManager
 
-from big_map_archive.generators import BMASecretLinks_excludes
+from big_map_archive.generators import (AnyUserWithSecretLink,
+                                        BMASecretLinks_excludes)
 
 
 class BMARecordPermissionPolicy(RDMRecordPermissionPolicy):
@@ -31,7 +36,6 @@ class BMARecordPermissionPolicy(RDMRecordPermissionPolicy):
     can_view = can_manage + [
         # AccessGrant("view"),
         SecretLinks("view"),
-        # SubmissionReviewer(),
         UserManager,
         CommunityInclusionReviewers(),
         RecordCommunitiesAction("view"),
@@ -107,3 +111,69 @@ class BMARecordPermissionPolicy(RDMRecordPermissionPolicy):
 
     # Disable lift of embargo (embargo is not allowed)
     can_lift_embargo = [SystemProcess()]
+
+
+class BMACommunityPermissionPolicy(CommunityPermissionPolicy):
+
+    # Important for community selection prior to record sharing
+    # Allow AnyUser with secret link to read communities info - needed to make records accessible via SecretLink by anyone
+    can_read = [AnyUserWithSecretLink(), CommunityMembers(), SystemProcess()]
+    # CommunityPermissionPolicy.can_read = [CommunityMembers(), SystemProcess()]
+
+    # Important for record sharing
+    # All communities should have an open record policy (possibility for publishing directly without review) and a restricted visibility
+    # record_policy is open => CommunityMembers
+    can_submit_record = [
+        IfPolicyClosed(
+            "record_policy",
+            then_=[Disable()],
+            else_=[
+                CommunityIfRestricted(
+                    "visibility",
+                    then_=[CommunityMembers()],
+                    else_=[Disable()],
+                ),
+            ],
+        ),
+    ]
+
+    # Community members are allowed to publish directly without review
+    # review_policy is open => CommunityMembers
+    can_include_directly = [
+        IfPolicyClosed(
+            "review_policy",
+            then_=[Disable()],
+            else_=[CommunityMembers()],
+        ),
+    ]
+
+    # Used for search filtering of deleted records
+    # CommunityPermissionPolicy.can_read_deleted = [
+    #     IfCommunityDeleted(then_=[SystemProcess()], else_=[SystemProcess()])
+    # ]
+    can_search = [AuthenticatedUser(), SystemProcess()]
+    can_search_invites = [SystemProcess()]
+    can_search_requests = [SystemProcess()]
+    can_rename = [SystemProcess()]
+    can_create = [SystemProcess()]
+    can_update = [SystemProcess()]
+    can_delete = [SystemProcess()]
+    can_purge = [SystemProcess()]
+    can_featured_search = [SystemProcess()]
+    can_featured_list = [SystemProcess()]
+    can_featured_create = [SystemProcess()]
+    can_featured_update = [SystemProcess()]
+    can_featured_delete = [SystemProcess()]
+    can_members_add = [SystemProcess()]
+    can_members_invite = [SystemProcess()]
+    can_members_manage = [SystemProcess()]
+    can_members_search = [SystemProcess()]
+    can_members_search_public = [SystemProcess()]
+    can_members_bulk_update = [SystemProcess()]
+    can_members_bulk_delete = [SystemProcess()]
+    can_members_update = [SystemProcess()]
+    can_members_delete = [SystemProcess()]
+    can_invite_owners = [SystemProcess()]
+    can_moderate = [Disable()]
+    can_manage_access = [SystemProcess()]
+    can_create_restricted = [SystemProcess()]
